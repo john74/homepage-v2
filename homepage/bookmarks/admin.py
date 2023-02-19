6cc5aedf363c4d46df5e2d75d5caac2b61d7c07f
JohnEf <eustathiou.ioan@protonmail.com>
2023-02-19 19:08:26 +0100

    create bookmark models.register models

--- homepage/bookmarks/admin.py
+++ homepage/bookmarks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BookmarkCategory, BookmarkSubCategory, Bookmark
+
+admin.site.register(BookmarkCategory)
+admin.site.register(BookmarkSubCategory)
+admin.site.register(Bookmark)
--- homepage/bookmarks/models.py
+++ homepage/bookmarks/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class BookmarkCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Bookmark Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class BookmarkSubCategory(models.Model):
+    category = models.ForeignKey(BookmarkCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Bookmark Subcategories'
+
+    def __str__(self):
+        return f'{self.category} - {self.name}'
+
+
+class Bookmark(models.Model):
+    sub_category = models.ForeignKey(BookmarkSubCategory, blank=True, null=True, on_delete=models.CASCADE, default='other')
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=700, unique=True)
+    icon = models.URLField(max_length=500, blank=True, null=True)
+    is_shortcut = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Bookmarks'
+
+    def __str__(self):
+        return f'{self.sub_category} - {self.name}'
