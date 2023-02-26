7911e9e061be344a057ceb7e66abf6eae8bea3b3
JohnEf <eustathiou.ioan@protonmail.com>
2023-02-26 12:51:54 +0100

    rearrange template files to extend index.html

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates/hompage',
+            BASE_DIR / 'templates/',
             BASE_DIR / 'home/templates/home',
             BASE_DIR / 'authentication/templates/authentication',
         ],
